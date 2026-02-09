0eab220e0c25cd241fe756baa2dc489b35d78e1e
aicaro1 <aicaro1@nait.ca>
2026-02-09 16:39:43 -0700

    react styling

--- 283-jsx-attributes-and-styling/src/index.jsx
+++ 283-jsx-attributes-and-styling/src/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 className="heading" contentEditable="true">My Favourite Foods</h1>
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
     </ul>
-  </div>,
-  document.getElementById("root")
+  </div>
 );
 
 // If you're running this locally in VS Code use the commands:

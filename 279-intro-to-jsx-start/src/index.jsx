a06555ff1593d930e703483cf3ddf5bac0a70c0a
aicaro1 <aicaro1@nait.ca>
2026-02-08 17:42:05 -0700

    ReactDOM.render()

--- 279-intro-to-jsx-start/src/index.jsx
+++ 279-intro-to-jsx-start/src/index.jsx
@@ -6,7 +6,7 @@
 
 //import dependency modules
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 //In order to use React in order to create something on screen, we will use the .render() method from the ReactDOM library. 
 
@@ -15,7 +15,12 @@ import ReactDOM from "react-dom/client";
 //ReactDOM.render(what to show, where to show it, callback function (optional) to tell us when the render is complete)
 
 ReactDOM.render(
-    <h1>Hello World!</h1>,
+    <div>
+        <h1>Hello World!</h1>
+        <p>This is a paragraph.</p>
+    </div>,
     document.getElementById("root")
 );
+
+
 
